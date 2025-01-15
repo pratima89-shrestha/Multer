debcdf07e2c13373fce201533615b13cdf70d561
pratima89-shrestha <crexthapratima111@gmail.com>
2025-01-16 02:17:10 +0545

    simple setup

--- multer_express/server.js
+++ multer_express/server.js
@@ -1,17 +1,35 @@
-//creating an application
-//This is the endpoint setting
+//yt.james Q Quick
 const express = require('express');
+const multer = require('multer');
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Hello");
-})
-
-app.post('api/upload',(req,res)=>{
-    res.send("uploaded successfully.");
+// Multer configuration
+const upload = multer({
+    dest: 'uploads/',
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype === 'image/jpeg') {
+            cb(null, true);
+        } else {
+            cb(new Error('Only .jpg files are allowed!'), false);
+        }
+    },
 });
 
-app.listen(4000,()=>{
-    console.log(`Listening on port 4000.`);
-})
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello!');
+});
 
+app.post('/api/upload', upload.single('file'), (req, res) => {
+    console.log('File:', req.file);
+    if (req.file) {
+        res.send('File uploaded successfully!');
+    } else {
+        res.status(400).send('No file uploaded.');
+    }
+});
+
+// Start server
+app.listen(4000, () => {
+    console.log('Listening on port 4000.');
+});
