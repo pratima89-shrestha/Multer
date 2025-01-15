a250366b6c895dff369072cf63577472309515ec
pratima89-shrestha <crexthapratima111@gmail.com>
2025-01-16 01:29:52 +0545

    entry point and app creation of express added

--- multer_express/server.js
+++ multer_express/server.js
@@ -1,1 +1,17 @@
+//creating an application
+//This is the endpoint setting
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Hello");
+})
+
+app.post('api/upload',(req,res)=>{
+    res.send("uploaded successfully.");
+});
+
+app.listen(4000,()=>{
+    console.log(`Listening on port 4000.`);
+})
 
